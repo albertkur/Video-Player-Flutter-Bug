471c151cf3072cf4f19c244f0b5be6cdf9a0c10d
albertkur <albertk1399@gmail.com>
2022-07-19 11:04:09 +0530

    minimum code

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: _BumbleBeeFileVideo()),
+      home: Scaffold(
+        body: _BumbleBeeFileVideo(),
+      ),
     );
   }
 }
@@ -32,11 +34,6 @@ class _BumbleBeeFileVideo extends StatefulWidget {
 class _BumbleBeeFileVideoState extends State<_BumbleBeeFileVideo> {
   VideoPlayerController? _controller;
 
-  // Future<ClosedCaptionFile> _loadCaptions() async {
-  //   final String fileContents = await DefaultAssetBundle.of(context).loadString('assets/bumble_bee_captions.vtt');
-  //   return WebVTTCaptionFile(fileContents); // For vtt files, use WebVTTCaptionFile
-  // }
-
   @override
   void initState() {
     super.initState();
@@ -45,10 +42,9 @@ class _BumbleBeeFileVideoState extends State<_BumbleBeeFileVideo> {
 
   Future<void> _initController() async {
     final File file =
-        await downloadFile('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4', 'bee.mp4');
+        await _downloadFile('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4', 'bee.mp4');
     _controller = VideoPlayerController.file(
       file,
-      // closedCaptionFile: _loadCaptions(),
       videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
     );
 
@@ -63,9 +59,9 @@ class _BumbleBeeFileVideoState extends State<_BumbleBeeFileVideo> {
     await _controller!.play();
   }
 
-  Future<File> downloadFile(String url, String name) async {
-    final String documentDirPath = (await path_provider.getApplicationSupportDirectory()).path;
-    // final String documentDirPath = (await path_provider.getApplicationDocumentsDirectory()).path;
+  Future<File> _downloadFile(String url, String name) async {
+    // final String documentDirPath = (await path_provider.getApplicationSupportDirectory()).path;
+    final String documentDirPath = (await path_provider.getApplicationDocumentsDirectory()).path;
 
     final String path = '$documentDirPath/$name';
 
@@ -82,146 +78,22 @@ class _BumbleBeeFileVideoState extends State<_BumbleBeeFileVideo> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(padding: const EdgeInsets.only(top: 20.0)),
-          const Text('With remote mp4'),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: _controller != null
-                ? AspectRatio(
-                    aspectRatio: _controller!.value.aspectRatio,
-                    child: Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        VideoPlayer(_controller!),
-                        ClosedCaption(text: _controller!.value.caption.text),
-                        // _ControlsOverlay(controller: _controller!),
-                        VideoProgressIndicator(_controller!, allowScrubbing: true),
-                      ],
-                    ),
-                  )
-                : null,
-          ),
-        ],
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: _controller != null
+            ? AspectRatio(
+                aspectRatio: _controller!.value.aspectRatio,
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: <Widget>[
+                    VideoPlayer(_controller!),
+                    VideoProgressIndicator(_controller!, allowScrubbing: true),
+                  ],
+                ),
+              )
+            : null,
       ),
     );
   }
 }
-
-// class _ControlsOverlay extends StatelessWidget {
-//   const _ControlsOverlay({Key? key, required this.controller}) : super(key: key);
-
-//   static const List<Duration> _exampleCaptionOffsets = <Duration>[
-//     Duration(seconds: -10),
-//     Duration(seconds: -3),
-//     Duration(seconds: -1, milliseconds: -500),
-//     Duration(milliseconds: -250),
-//     Duration(milliseconds: 0),
-//     Duration(milliseconds: 250),
-//     Duration(seconds: 1, milliseconds: 500),
-//     Duration(seconds: 3),
-//     Duration(seconds: 10),
-//   ];
-//   static const List<double> _examplePlaybackRates = <double>[
-//     0.25,
-//     0.5,
-//     1.0,
-//     1.5,
-//     2.0,
-//     3.0,
-//     5.0,
-//     10.0,
-//   ];
-
-//   final VideoPlayerController controller;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: <Widget>[
-//         AnimatedSwitcher(
-//           duration: const Duration(milliseconds: 50),
-//           reverseDuration: const Duration(milliseconds: 200),
-//           child: controller.value.isPlaying
-//               ? const SizedBox.shrink()
-//               : Container(
-//                   color: Colors.black26,
-//                   child: const Center(
-//                     child: Icon(
-//                       Icons.play_arrow,
-//                       color: Colors.white,
-//                       size: 100.0,
-//                       semanticLabel: 'Play',
-//                     ),
-//                   ),
-//                 ),
-//         ),
-//         GestureDetector(
-//           onTap: () {
-//             controller.value.isPlaying ? controller.pause() : controller.play();
-//           },
-//         ),
-//         Align(
-//           alignment: Alignment.topLeft,
-//           child: PopupMenuButton<Duration>(
-//             initialValue: controller.value.captionOffset,
-//             tooltip: 'Caption Offset',
-//             onSelected: (Duration delay) {
-//               controller.setCaptionOffset(delay);
-//             },
-//             itemBuilder: (BuildContext context) {
-//               return <PopupMenuItem<Duration>>[
-//                 for (final Duration offsetDuration in _exampleCaptionOffsets)
-//                   PopupMenuItem<Duration>(
-//                     value: offsetDuration,
-//                     child: Text('${offsetDuration.inMilliseconds}ms'),
-//                   )
-//               ];
-//             },
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 // Using less vertical padding as the text is also longer
-//                 // horizontally, so it feels like it would need more spacing
-//                 // horizontally (matching the aspect ratio of the video).
-//                 vertical: 12,
-//                 horizontal: 16,
-//               ),
-//               child: Text('${controller.value.captionOffset.inMilliseconds}ms'),
-//             ),
-//           ),
-//         ),
-//         Align(
-//           alignment: Alignment.topRight,
-//           child: PopupMenuButton<double>(
-//             initialValue: controller.value.playbackSpeed,
-//             tooltip: 'Playback speed',
-//             onSelected: (double speed) {
-//               controller.setPlaybackSpeed(speed);
-//             },
-//             itemBuilder: (BuildContext context) {
-//               return <PopupMenuItem<double>>[
-//                 for (final double speed in _examplePlaybackRates)
-//                   PopupMenuItem<double>(
-//                     value: speed,
-//                     child: Text('${speed}x'),
-//                   )
-//               ];
-//             },
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 // Using less vertical padding as the text is also longer
-//                 // horizontally, so it feels like it would need more spacing
-//                 // horizontally (matching the aspect ratio of the video).
-//                 vertical: 12,
-//                 horizontal: 16,
-//               ),
-//               child: Text('${controller.value.playbackSpeed}x'),
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
